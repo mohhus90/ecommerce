24881e78ea7846fa5a32cd79013ae190c04eeeff
mohhus90 <mohhus90@gmail.com>
2023-07-18 06:03:18 +0300

    modified view of add and edit

--- admin/includes/tmpl/footer.php
+++ admin/includes/tmpl/footer.php
@@ -3,8 +3,7 @@
            
         </div>
         <script src="layout/js/jquery-3.7.0.min.js"></script>
-        <script src="layout/js/bootstrap.min.js"></script>
-        <script src="layout/js/backend.js"></script>
+        <script src="layout/js/bootstrap.min.js"></script> 
         <!-- <script src="layout/js/bootstrap.js"></script> -->
         <script src="layout/js/bootstrap.bundle.min.js"></script>
         
@@ -29,5 +28,6 @@
                 }
             });
         </script>
+        <script src="layout/js/backend.js"></script>
     </body>
 </html>
